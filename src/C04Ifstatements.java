759ace9613061b628ba17d1eafd00c0393b399b9
Hi-Imjaeyoung <imjy2757@naver.com>
2023-12-06 12:40:40 +0900

    java if

---
+++ src/C04Ifstatements.java
@@ -0,0 +1,69 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class C04Ifstatements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        String inputPassWd = sc.nextLine();
+//        String nowPassWd = "1234";
+//        if(inputPassWd.equals(nowPassWd)){
+//            System.out.println("오세요~");
+//        }else{
+//            System.out.println("나가~");
+//        }
+//        묵시적 타입변환, 각 문자가 숫자(아스키 코드)를 가짐
+//        a : 97 b :98 , A :65
+//        사용자에게 아무 알파벳 하나를 입력 받아서 대소문자 판단하기
+//        char inputAlpha = sc.nextLine().charAt(0);
+//        if(inputAlpha<97){  //(input <'a')
+//            System.out.println("대문자군요");
+//        }else{
+//            System.out.println("소문자군요");
+//        }
+//
+//        if(Character.isUpperCase(inputAlpha)){
+//            System.out.println("대문자군요");
+//        }else{
+//            System.out.println("소문자군요");
+//        }
+//        버스 카드 에제 
+//        System.out.println("----버스카드 예제------");
+////        요금 2700원 보다 적으면 x 많으면 O
+////        도난 여부를 boolean으로 설정
+//        int myMoney = sc.nextInt();
+//        boolean still = false;
+//        if(myMoney<2700 || still){ // or조건을 이용하여 구현
+//            System.out.println("못타");
+//        }else {
+//            System.out.println("타");
+//        }
+//        삼항연산자
+//        System.out.println("----삼항 연산자----");
+//        int nowPassWd = 1234;
+//        int inputPassWd = sc.nextInt();
+//        String result = (inputPassWd==nowPassWd)? "오세요!" : "나가~"; //if else를 간략하게 쓰고 싶을 때
+//        System.out.println(result);
+
+//    택시 만원 , 버스 3천 , 킥보드 2천 , 걸어 0
+//
+//    switch
+//        System.out.println("------switch문 예제------");
+//        switch (sc.nextInt()){
+//            case 1:
+//                System.out.println("대출 사비스 입다");
+//                break;
+//            case 2:
+//                System.out.println("예금");
+//                break;
+//            case 3:
+//                System.out.println("적금");
+//                break;
+//            case 0:
+//                System.out.println("상담사 연결");
+//                break;
+//            default:
+//                System.out.println("당신 틀렸어");
+//                break;
+//        }
+    }
+}
