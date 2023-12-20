a5426c2ba62f415009089c9dc37f2e79fc62b4ca
Hi-Imjaeyoung <imjy2757@naver.com>
2023-12-20 17:41:47 +0900

    java Interface practice
    ArrayList와 LinkedList의 차이점

---
+++ src/C14Interface/ArrayListLinkedListCompare.java
@@ -0,0 +1,26 @@
+package C14Interface;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ArrayListLinkedListCompare {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        List<Integer> myList = new ArrayList<>();
+        for(int i=0;i<100000;i++) {
+            myList.add(0,10);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime-startTime);
+
+
+        long startTime2 = System.currentTimeMillis();
+        for(int i=0;i<10000000;i++) {
+            myList.get(i);
+        }
+        long endTime2 = System.currentTimeMillis();
+        System.out.println(endTime2-startTime2);
+    }
+}
