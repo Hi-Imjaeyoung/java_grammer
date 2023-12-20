c1bb5719473ec01d11a6c3af2296fb6a09a29338
Hi-Imjaeyoung <imjy2757@naver.com>
2023-12-20 17:42:06 +0900

    java Interface practice

--- src/C14Interface/CatImplements.java
+++ src/C14Interface/CatImplements.java
@@ -6,3 +6,14 @@ public class CatImplements implements AnimalInterface1{
         System.out.println("골골골..");
     }
 }
+class CatMultiImplements implements AnimalInterface1,AnimalInterface2{
+    @Override
+    public void makeSound() {
+        System.out.println("골골골..");
+    }
+
+    @Override
+    public String play(String a, String b) {
+        return a+" 와 "+b+"가 논다!";
+    }
+}
--- src/C14Interface/DogImplements.java
+++ src/C14Interface/DogImplements.java
@@ -6,3 +6,14 @@ public class DogImplements implements AnimalInterface1{
         System.out.println("mong mong ");
     }
 }
+class DogMultiImplements implements AnimalInterface1,AnimalInterface2{
+    @Override
+    public void makeSound() {
+        System.out.println("mong mong ");
+    }
+
+    @Override
+    public String play(String a, String b) {
+        return a+" 와 "+b+"가 논다!";
+    }
+}
