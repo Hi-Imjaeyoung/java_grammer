fc664a89df9a5f068e35c3f9d914294333552e4b
Hi-Imjaeyoung <imjy2757@naver.com>
2023-12-26 12:27:37 +0900

    java Calendar upload

--- src/C16EtcClass/C1602Calendar.java
+++ src/C16EtcClass/C1602Calendar.java
@@ -3,6 +3,7 @@ package C16EtcClass;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
 import java.util.Calendar;
 
 public class C1602Calendar {
@@ -32,5 +33,27 @@ public class C1602Calendar {
 
         LocalDateTime presentLocalDateTime = LocalDateTime.now();
         System.out.println(presentLocalDateTime);
+
+        // 암의로 특정 시간을 만들어 내고 싶을 때 : of 메서드 사용
+        LocalDate brithDay = LocalDate.of(1996,9,4);
+        // 일반 내장 메서드를 사용
+        System.out.println(brithDay.getYear());
+        System.out.println(brithDay.getMonth());
+        System.out.println(brithDay.getDayOfMonth());
+        // of는 메서드오버로딩이 되어서 sec 생략가능
+        LocalTime brithTime = LocalTime.of(18,02,19);
+        LocalDateTime brithDaytime= LocalDateTime.of(brithDay,brithTime);
+
+        // 크로노필드 enum타입 사용 : 매개변수로 크로노필드를 받아 프로그램의 유연성 향상
+        System.out.println(brithDaytime.get(ChronoField.YEAR));
+        System.out.println(brithDaytime.get(ChronoField.MONTH_OF_YEAR));
+        // 0 : am , 1 :pm
+        System.out.println(brithDaytime.get(ChronoField.AMPM_OF_DAY));
+        LocalTime nowTime = LocalTime.now();
+        if(nowTime.get(ChronoField.AMPM_OF_DAY)==0){
+            System.out.println("현재는 오전 입니다 졸리군요");
+        }else{
+            System.out.println("현재는 오후 입니다 집에 갑시다");
+        }
     }
 }
